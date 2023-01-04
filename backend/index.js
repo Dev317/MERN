934b571c1b296a34248ae38e988e39953d44b30c
Minhhhh <mineskiroxor@gmail.com>
2023-01-04 17:54:27 +0800

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
+const CONNECTION_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.c8uzcdh.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+    .connect(
+        CONNECTION_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
